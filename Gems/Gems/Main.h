525570c554a607522c539b302eb86db26c25d4eb
Dmitry <KhripunkovDV@mail.ru>
2020-05-01 20:00:02 +0300

    Final build
    Gems v1.0

--- Gems/Gems/Main.h
+++ Gems/Gems/Main.h
@@ -2,11 +2,11 @@
 
 constexpr unsigned BONUS_CHANCE = 5;    /**Chance to spawn bonus for every gem destroyed at once*/
 constexpr unsigned BONUS_COUNT = 2; /**Amount of bonuses available */
-constexpr unsigned FRAME_RATE = 3;  /**How many frames change in a second*/
+constexpr unsigned FRAME_RATE = 5;  /**How many frames change in a second*/
 constexpr unsigned BONUS_SPAWN_RADIUS = 3;  /*How far can bonus spawn from original*/
 
 /**
-*Special enum for every entity type
+*Special enum for every cell entity type
 *
 *Mainly used to load and keep textures for every type of tile
 */
